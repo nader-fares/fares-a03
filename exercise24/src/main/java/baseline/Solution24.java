eaf023162a11fc5a4269f7353d67ea4b527ab09c
Nader Fares <naderfares@outlook.com>
2021-09-29 17:18:03 -0400

    skeleton for exercise 24

---
+++ exercise24/src/main/java/baseline/Solution24.java
@@ -0,0 +1,43 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Nader Fares
+ */
+
+/*
+Create a program that compares two strings and determines if the two strings are anagrams.
+The program should prompt for both input strings and
+display the output as shown in the example that follows.
+ */
+package baseline;
+
+public class Solution24 {
+    public static void main(String[] args) {
+        Solution24 app = new Solution24();
+
+        //display directions
+
+        //declare stringA and stringB
+        String firstString;
+        String secondString;
+
+        //ask user for first string
+        //ask user for second string
+
+        //if isAnagram is true print are anagrams
+        //else print are not anagrams
+    }
+
+    public static void displayDirections() {
+    //print directions
+        return;
+    }
+
+    //check if two strings are anagrams
+    private boolean isAnagram(String stringA, String stringB) {
+        //get length of both strings and compare length
+        //convert strings to character arrays
+        //sort character arrays
+        //compare character arrays
+        return true;
+    }
+}
