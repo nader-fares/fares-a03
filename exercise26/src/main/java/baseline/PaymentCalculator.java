8c28928db8b08a58f177f9f0b1d0aa3e636ef454
Nader Fares <naderfares@outlook.com>
2021-09-29 21:57:01 -0400

    added skeleton for exercise26

---
+++ exercise26/src/main/java/baseline/PaymentCalculator.java
@@ -0,0 +1,19 @@
+package baseline;
+
+public class PaymentCalculator {
+    //declare instance variables
+    double balance;
+    double apr;
+    double monthlyPayment;
+
+    //declare constructor
+    PaymentCalculator(double balance, double apr, double monthlyPayment) {
+    }
+
+    //declare instance methods
+    public int calculateMonthsUntilPaidOff() {
+        //calculate months using formula
+        //Math.log method to obtain log
+        return 1;
+    }
+}
