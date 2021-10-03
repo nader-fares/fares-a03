b44f091328f92a7b7a500be3e60f022ecdfc2413
Nader Fares <naderfares@outlook.com>
2021-10-03 15:18:22 -0400

    add skeleton code for exercise 28

---
+++ exercise28/src/main/java/baseline/Solution28.java
@@ -0,0 +1,31 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Nader Fares
+ */
+
+/*
+Write a program that prompts the user for five numbers and computes the total of the numbers.
+
+Constraints
+    The prompting must use repetition, such as a counted loop, not three separate prompts.
+ */
+
+package baseline;
+
+public class Solution28 {
+    // declare total instance variable
+    public int total = 0;
+    public static void main(String[] args) {
+//        loop 5 times
+//        for each iteration: prompt number and add it to the total
+    }
+
+    public int promptNumber() {
+//        prompt the user for a number
+//        return user input
+    }
+
+    public void addToTotal(int num) {
+//        add number to the total
+    }
+}
