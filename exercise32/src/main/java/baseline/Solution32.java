786fae8c6fd2522e1c861b336ffe385e41934709
Nader Fares <naderfares@outlook.com>
2021-10-03 18:53:35 -0400

    add skeleton code for exercise 32

---
+++ exercise32/src/main/java/baseline/Solution32.java
@@ -0,0 +1,46 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Nader Fares
+ */
+
+/*
+Write a Guess the Number game that has three levels of difficulty. The first level of difficulty would be a number between 1 and 10. The second difficulty set would be between 1 and 100. The third difficulty set would be between 1 and 1000.
+
+Prompt for the difficulty level, and then start the game. The computer picks a random number in that range and prompts the player to guess that number. Each time the player guesses, the computer should give the player a hint as to whether the number is too high or too low. The computer should also keep track of the number of guesses. Once the player guesses the correct number, the computer should present the number of guesses and ask if the player would like to play again.
+Constraints
+Don’t allow any non-numeric data entry.
+During the game, count non-numeric entries as wrong guesses.
+ */
+
+package baseline;
+
+import java.util.Scanner;
+
+public class Solution32 {
+    private static final Scanner input = new Scanner(System.in);
+
+    public int randomNumber;
+
+
+    public static void main(String[] args) {
+        Solution32 app = new Solution32();
+//        create a variable to store number of guesses
+//        prompt user for difficulty
+//        determine number range
+//        generate a random number
+//        loop to prompt user for guesses
+    }
+
+    public void generateRandomNumber(int difficulty) {
+//        declare max int
+        int max;
+//        choose max int based on difficulty
+//        change random number var
+
+    }
+
+    public int promptNumber(String promptMessage) {
+//        prompt user for number
+//        if invalid, return 0 (always incorrect)
+    }
+}
