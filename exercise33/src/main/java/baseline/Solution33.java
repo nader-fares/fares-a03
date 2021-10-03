61d08b37a9dfa68a14612fb78dfaec3aa463c9ab
Nader Fares <naderfares@outlook.com>
2021-10-03 19:36:16 -0400

    add skeleton code for exercise 33

---
+++ exercise33/src/main/java/baseline/Solution33.java
@@ -0,0 +1,27 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Nader Fares
+ */
+
+/*
+Create a Magic 8 Ball game that prompts for a question and then displays either "Yes," "No," "Maybe," or "Ask again later."
+Constraint
+The value should be chosen randomly using a pseudo random number generator. Store the possible choices in a list and select one at random.
+ */
+
+package baseline;
+
+public class Solution33 {
+    public static void main(String[] args) {
+//        create array of responses
+        String[] responses = {"Yes", "No", "Maybe", "Ask again later."};
+//        prompt user for question
+//        output a random response from list
+
+    }
+
+    public String getRandomResponse(String responses[]) {
+        // generate a random number between 1 and 3
+        // get a random response from the list and return
+    }
+}
