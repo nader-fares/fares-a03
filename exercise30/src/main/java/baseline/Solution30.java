de49ef4aabcfa9cbf5ce67f1872ddb0416f4a4ca
Nader Fares <naderfares@outlook.com>
2021-10-03 16:20:11 -0400

    add skeleton code for exercise 30

---
+++ exercise30/src/main/java/baseline/Solution30.java
@@ -0,0 +1,29 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Nader Fares
+ */
+
+/*
+Create a program that generates a multiplication table for the numbers 1 through 12 (inclusive).
+Constraint
+Use a nested loop to complete this program.
+Align each column within the table without using the tab character.
+ */
+
+package baseline;
+
+public class Solution30 {
+    public static void main(String[] args) {
+        Solution30 app = new Solution30();
+//        loop 1 through 12 and print a row for each number as the multiplier
+    }
+
+    public void printRowString(int multiplier) {
+//        declare empty row string
+        String rowString = "";
+//        loop 1 through 12
+
+//        concatenate the product each number and the multiplier to the string
+//        print the row string
+    }
+}
