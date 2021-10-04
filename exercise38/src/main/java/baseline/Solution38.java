09add1bd8f843c9349d587ffbf43f22cdc115251
Nader Fares <naderfares@outlook.com>
2021-10-03 22:30:11 -0400

    add skeleton code for exercise 38

---
+++ exercise38/src/main/java/baseline/Solution38.java
@@ -0,0 +1,31 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Nader Fares
+ */
+
+/*
+Create a program that prompts for a list of numbers, separated by spaces. Have the program print out a new list containing only the even numbers.
+Constraints
+Convert the input to an array. Many languages can easily convert strings to arrays with a built-in function that splits apart a string based on a specified delimiter.
+Write your own algorithm—don’t rely on the language’s built-in filter or similar enumeration feature.
+Use a function called filterEvenNumbers to encapsulate the logic for this. The function takes in the original array and returns the new array.
+ */
+
+package baseline;
+
+public class Solution38 {
+    public static void main(String[] args) {
+//        ask user for number string
+//        convert string to array and filter
+//        convert new array to string
+//        return new string
+    }
+
+    public String[] stringToArray(String str) {
+//        split the string by the space character
+    }
+
+    public String arrayToString(String[] arr) {
+//        create an array from the string
+    }
+}
