108c5510316954e57e35c6fef342658f8707f498
Nader Fares <naderfares@outlook.com>
2021-10-03 23:01:40 -0400

    add skeleton code for exercise 39

---
+++ exercise39/src/main/java/baseline/Solution39.java
@@ -0,0 +1,24 @@
+package baseline;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+public class Solution39 {
+    public static ArrayList<Map<String, String>> employees = new ArrayList<Map<String, String>>();
+    Solution39() {
+//        populate the list of maps
+    }
+    public static void main(String[] args) {
+//        declare sorted array value
+//        call sort method
+//        loop through list and print rows
+    }
+
+    public void getSortedArray() {
+        // sort list by comparing last names
+    }
+
+    public String getRowString(Map<String, String> employee) {
+//        return formatted string with employee unfo
+    }
+}
