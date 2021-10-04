99fb86b04fda34e1b5e1a4c20e9a983b9fbb5aa7
Nader Fares <naderfares@outlook.com>
2021-10-03 20:37:49 -0400

    add skeleton code for exercise 36

---
+++ exercise36/src/main/java/baseline/Solution36.java
@@ -0,0 +1,51 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Nader Fares
+ */
+
+/*
+Write a program that prompts for response times from a website in milliseconds. It should keep asking for values until the user enters “done.”
+
+The program should print the average time (mean), the minimum time, the maximum time, and the population standard deviation.
+
+Constraints
+Create functions called average, max, min, and std, which take in a list of numbers and return the results.
+Use loops and arrays to perform the input and mathematical operations.
+Be sure to exclude the "done" entry from the inputs.
+Be sure to properly ignore any invalid inputs.
+Keep the input separate from the processing and the output.
+ */
+
+package baseline;
+
+public class Solution36 {
+    public static void main(String[] args) {
+//      loop
+    }
+
+    public String promptMilliseconds() {
+//        ask user for a number
+//        check if it's "done"
+//        check if it's not a number and loop if it isnt
+//        return milliseconds as string
+    }
+
+    public double getMean() {
+//        get sum of values and divide by the size of the responseTimes array
+    }
+
+    public int getMin() {
+//        return the minimum value in the array
+    }
+
+    public int getMax() {
+//        return the maximum value in the array
+    }
+
+    public double getStandardDeviation() {
+//        create sum variable
+//        loop through array to calculate each value's difference from the mean and add to sum
+//        divide by list size and return
+
+    }
+}
