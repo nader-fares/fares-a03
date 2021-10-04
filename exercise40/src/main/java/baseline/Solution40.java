ddcb3cdcd6d252e39dc15d4e7fe9da1413b58346
Nader Fares <naderfares@outlook.com>
2021-10-03 23:30:19 -0400

    add skeleton code for exercise 40

--- exercise40/src/main/java/baseline/Solution40.java
+++ exercise40/src/main/java/baseline/Solution40.java
@@ -1,2 +1,33 @@
-package baseline;public class Solution40 {
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Nader Fares
+ */
+package baseline;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Solution40 {
+    private static final Scanner input = new Scanner(System.in);
+    public ArrayList<Map<String, String>> employees = new ArrayList<Map<String, String>>();
+    Solution40() {
+//        populate the list of maps
+
+    }
+
+    public static void main(String[] args) {
+        // call filter method
+
+        // loop through array and print each employee row
+    }
+    public void filterArray(String userInput) {
+        // filter array by user input
+        // check if first name or last name contain the input
+    }
+
+    public String getRowString(Map<String, String> employee) {
+//        return formatted string with employee info
+    }
 }
